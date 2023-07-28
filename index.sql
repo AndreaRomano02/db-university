e9fe537cd027043513534a8abc12657e1d88b7df
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-28 13:55:59 +0200

    Add request 8

--- index.sql
+++ index.sql
@@ -47,6 +47,9 @@ SELECT COUNT(*) FROM `departments`;
 
 --* 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NULL;
 
 --------------------------------------------------------------------------------------------------------------------------
 
