621c4ffd3a678f28e7e31a4cf191fdbd60eee2f6
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-28 14:12:00 +0200

    Add request 4 GROUP BY

--- index.sql
+++ index.sql
@@ -76,4 +76,8 @@ GROUP BY `exam_id`;
 
 --* 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
+SELECT `department_id`, COUNT(`name`) AS `n_of_departments`
+FROM `degrees`
+GROUP BY `department_id`;
+
 --------------------------------------------------------------------------------------------------------------------------
