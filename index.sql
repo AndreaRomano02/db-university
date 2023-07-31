fd130b55b024501faec8792be5fba86114540688
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-31 13:42:16 +0200

    Add REQUEST JOIN 1

--- index.sql
+++ index.sql
@@ -87,6 +87,11 @@ GROUP BY `department_id`;
 
 --* 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+SELECT S.`name`, S. `surname`, S.`registration_number`
+FROM `degrees` AS D
+JOIN `students` AS S
+ON D . `id` = S . `degree_id`
+WHERE D. `name` = 'Corso di Laurea in Economia';
 
 --* 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
